3868e1549e95ae8bd21cb961443c2aafcdb9801f
firatarslan <firatarslan@dreamharvesters.com>
2019-10-10 18:35:17 +0300

    code refactoring

--- Assets/CCEvent.cs
+++ Assets/CCEvent.cs
@@ -5,33 +5,27 @@ using UnityEngine;
 public class CCEvent : MonoBehaviour
 {
     Renderer rend;
-    
+
     void Start()
     {
         rend = GetComponent<Renderer>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-       
-    }
-
-
     private void OnTriggerEnter(Collider collider)
     {
-        if (collider.gameObject.tag == "Player")
-        {
-            rend.material.SetColor("_Color",Color.green);
-        }
+        SetRendererColorIfTagIsPlayer(collider, Color.green);
     }
 
     void OnTriggerExit(Collider collider)
     {
-        if (collider.gameObject.tag == "Player")
+        SetRendererColorIfTagIsPlayer(collider, Color.red);
+    }
+
+    private void SetRendererColorIfTagIsPlayer(Collider collider, Color color)
+    {
+        if (collider.CompareTag("Player"))
         {
-            rend.material.SetColor("_Color", Color.red);
+            rend.material.SetColor("_Color", color);
         }
     }
 }
-
